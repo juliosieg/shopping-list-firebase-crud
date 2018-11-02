c405ea73980836e94ef4fe15d43017d9efdf33de
Julio Sieg <julio.sieg@gmail.com>
2018-11-02 00:18:25 -0300

    alterações

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB3rQSDbVL1kxzgVuGKliCI-8L3ZD-SCM4",
+    authDomain: "shopping-list-1171b.firebaseapp.com",
+    databaseURL: "https://shopping-list-1171b.firebaseio.com",
+    projectId: "shopping-list-1171b",
+    storageBucket: "shopping-list-1171b.appspot.com",
+    messagingSenderId: "682721921189"
+};
